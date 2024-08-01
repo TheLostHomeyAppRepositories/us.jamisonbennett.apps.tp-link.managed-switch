50aa38087073398c803328e86d3a6f806e6955b5
Jamison Bennett <jamison.bennett@gmail.com>
2024-07-31 21:35:59 -0500

    Add default port to the device settings
    Add a default port to the device settings so that the homey device can
    be used to easily configure a favorite port rather than port 1.

--- drivers/tp-link-managed-switch/device.ts
+++ drivers/tp-link-managed-switch/device.ts
@@ -16,6 +16,15 @@ class Device extends Homey.Device {
       this.log("Unable to connect to managed switch");
     }
 
+    const defaultCapability = "onoff.default";
+    await this.addCapability(defaultCapability);
+    this.registerCapabilityListener(defaultCapability, this.onCapabilityOnoffDefault.bind(this));
+
+    const defaultTitle = this.homey.__(`settings.drivers.tp-link-managed-switch.defaultPort`);
+    await this.setCapabilityOptions(defaultCapability, {
+      title: defaultTitle
+    });
+
     for (let i = 1; i <= this.deviceAPI.getNumPorts(); i++ ) {
       this.log(`Loading switch port ${i}`);
       const capability = `onoff.${i}`;
@@ -37,6 +46,47 @@ class Device extends Homey.Device {
     const result = await this.deviceAPI.setPortEnabled(port, value);
     if (!result) {
       this.log(`Unable to set the port ${port} ${value ? 'on' : 'off'}`);
+    }
+  }
+
+  async onCapabilityOnoffDefault(value: boolean) {
+    const defaultPortNumber = this.getSetting('default_port_number') || 0;
+    this.log(`Turning the default switch port ${defaultPortNumber} ${value ? 'on' : 'off'}`);
+    if (defaultPortNumber == 0) {
+      // There is no default port
+      return;
+    }
+
+    return this.onCapabilityOnoff(defaultPortNumber, value);
+  }
+
+  async onSettings({ oldSettings, newSettings, changedKeys }: { oldSettings: any, newSettings: any, changedKeys: string[] }) {
+
+    if (changedKeys.includes('default_port_number')) {
+      // Ensure the device actually has the port number on it
+      try {
+        const newDefaultPortNumber = newSettings.default_port_number;
+        if (!Number.isInteger(newDefaultPortNumber)) {
+          throw new Error('Non-integer port number are not supported.');
+        }
+        if (newDefaultPortNumber < 0) {
+          throw new Error('Negative port numberr are not supported');
+        }
+        if (this.deviceAPI != null) {
+          const maxPortNumber = this.deviceAPI.getNumPorts();
+          if (newDefaultPortNumber > maxPortNumber) {
+            throw new Error(`The maximum port number on this device is ${this.deviceAPI.getNumPorts()}.`);
+          }
+        }
+      } catch (error) {
+        if (error instanceof Error) {
+          this.log("Invalid default port number:", error.message);
+          throw new Error(`Invalid default port number ${error.message}`);
+        } else {
+          this.log("Invalid default port number");
+          throw new Error("Invalid default port number");
+        }
+      }
     }
   }
 }
