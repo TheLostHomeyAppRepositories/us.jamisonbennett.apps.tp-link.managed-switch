05b50741e9bf5781b1ccd70db262d24f6bee98af
Jamison Bennett <jamison.bennett@gmail.com>
2024-08-01 22:05:52 -0500

    Add configurable ports setting to restrict certain ports
    Adds the configurable ports setting. This allows the managed switch
    settings to specify which switch ports should be able to be
    controlled. This can help prevent accidentially disabling all ports,
    accidentially disabling the port used to manage the switch, etc.

--- drivers/tp-link-managed-switch/device.ts
+++ drivers/tp-link-managed-switch/device.ts
@@ -5,6 +5,8 @@ class Device extends Homey.Device {
 
   private deviceAPI: DeviceAPI | null = null
 
+  private configurablePorts: boolean[] | null = null
+
   async onInit() {
     this.log('TP-Link managed switch device has been initialized');
 
@@ -36,16 +38,25 @@ class Device extends Homey.Device {
         title: title
       });
     }
+
+    await this.handleConfigurablePortsChange(this.getSetting('configurable_ports'));
   }
 
   async onCapabilityOnoff(port: number, value: boolean) {
     this.log(`Turning switch port ${port} ${value ? 'on' : 'off'}`);
     if (this.deviceAPI == null) {
-      return;
+      this.log(`Unable to set the port ${port} ${value ? 'on' : 'off'} because the device is not initialized.`);
+      throw new Error(`Unable to set the port ${port} ${value ? 'on' : 'off'} because the device is not initialized.`);
     }
-    const result = await this.deviceAPI.setPortEnabled(port, value);
-    if (!result) {
-      this.log(`Unable to set the port ${port} ${value ? 'on' : 'off'}`);
+    if (this.configurablePorts == null || this.configurablePorts[port-1]) {
+      const result = await this.deviceAPI.setPortEnabled(port, value);
+      if (!result) {
+        this.log(`Unable to set the port ${port} ${value ? 'on' : 'off'}`);
+        throw new Error(`Unable to set the port ${port} ${value ? 'on' : 'off'}`);
+      }
+    } else {
+      this.log(`Unable to set the port ${port} ${value ? 'on' : 'off'} because it was configuration is restricted.`);
+      throw new Error(`Unable to set the port ${port} ${value ? 'on' : 'off'} because it was configuration is restricted.`);
     }
   }
 
@@ -63,31 +74,88 @@ class Device extends Homey.Device {
   async onSettings({ oldSettings, newSettings, changedKeys }: { oldSettings: any, newSettings: any, changedKeys: string[] }) {
 
     if (changedKeys.includes('default_port_number')) {
-      // Ensure the device actually has the port number on it
-      try {
-        const newDefaultPortNumber = newSettings.default_port_number;
-        if (!Number.isInteger(newDefaultPortNumber)) {
-          throw new Error('Non-integer port number are not supported.');
-        }
-        if (newDefaultPortNumber < 0) {
-          throw new Error('Negative port numberr are not supported');
-        }
-        if (this.deviceAPI != null) {
-          const maxPortNumber = this.deviceAPI.getNumPorts();
-          if (newDefaultPortNumber > maxPortNumber) {
-            throw new Error(`The maximum port number on this device is ${this.deviceAPI.getNumPorts()}.`);
-          }
-        }
-      } catch (error) {
-        if (error instanceof Error) {
-          this.log("Invalid default port number:", error.message);
-          throw new Error(`Invalid default port number ${error.message}`);
-        } else {
-          this.log("Invalid default port number");
-          throw new Error("Invalid default port number");
+      this.handleDefaultPortChange(newSettings.default_port_number);
+    }
+
+    if (changedKeys.includes('configurable_ports')) {
+      this.handleConfigurablePortsChange(newSettings.configurable_ports);
+    }
+  }
+
+  private handleDefaultPortChange(newDefaultPortNumber: any) {
+    // Ensure the device actually has the port number on it
+    try {
+      if (!Number.isInteger(newDefaultPortNumber)) {
+        throw new Error('Non-integer port number are not supported.');
+      }
+      if (newDefaultPortNumber < 0) {
+        throw new Error('Negative port numberr are not supported');
+      }
+      if (this.deviceAPI != null) {
+        const maxPortNumber = this.deviceAPI.getNumPorts();
+        if (newDefaultPortNumber > maxPortNumber) {
+          throw new Error(`The maximum port number on this device is ${this.deviceAPI.getNumPorts()}.`);
         }
       }
+    } catch (error) {
+      if (error instanceof Error) {
+        this.log("Invalid default port number:", error.message);
+        throw new Error(`Invalid default port number ${error.message}`);
+      } else {
+        this.log("Invalid default port number");
+        throw new Error("Invalid default port number");
+      }
     }
+  }
+
+  private handleConfigurablePortsChange(newPorts: any) {
+    if (newPorts) {
+      const ports = this.parsePortNumbers(newPorts);
+      if (this.deviceAPI != null) {
+        const maxPortNumber = this.deviceAPI.getNumPorts();
+        let configurablePorts: boolean[] = new Array(maxPortNumber).fill(false);
+        ports.forEach(port => {
+          if (port <= 0 || port > maxPortNumber) {
+            throw new Error(`Port number out of range: ${port}`);
+          }
+          configurablePorts[port-1] = true;
+        });
+        this.configurablePorts = configurablePorts;
+      } else {
+        this.configurablePorts = null;
+      }
+    } else {
+      this.configurablePorts = null;
+    }
+  }
+
+  private parsePortNumbers(input: string): number[] {
+    if (!input) {
+      return []; // Empty value indicates all ports
+    }
+
+    const ports: number[] = [];
+    const ranges = input.replace(/\s+/g, '').split(',');
+
+    ranges.forEach(range => {
+      const [start, end] = range.split('-').map(Number);
+      if (!Number.isInteger(start) || (end !== undefined && !Number.isInteger(end))) {
+        throw new Error(`Invalid port range: ${range}`);
+      }
+
+      if (end === undefined) {
+        ports.push(start);
+      } else {
+        if (start > end) {
+          throw new Error(`Invalid range: ${range}`);
+        }
+        for (let i = start; i <= end; i++) {
+          ports.push(i);
+        }
+      }
+    });
+
+    return ports;
   }
 }
 
